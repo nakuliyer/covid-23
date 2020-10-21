177e58e7283ef2592d958c567b1c3a662a0a8fe6
Sri Abbineni <67569360+SriAbbineni17@users.noreply.github.com>
2020-10-21 15:37:25 -0500

    Add files via upload

--- backend/State/covid-data-request.py
+++ backend/State/covid-data-request.py
@@ -1,10 +1,21 @@
 import requests 
-r = requests.get('https://api.covidtracking.com/v1/states/current.json')
+import matplotlib.pyplot as plt
+r = requests.get('https://api.covidtracking.com/v1/states/il/daily.json')
 j = r.json()
 str1 = ""
 str2 = ""
+xl = []
+yl = []
 for d in j:
     for key, value in d.items():
+        if key == "date":
+            xl.append(value)
+        if key == "positive":
+            yl.append(value)
         str2 = str(key) + ' : ' + str(value) + '\n'
         str1 += str2 
-print(str1)
+#print(str1)
+plt.plot(xl, yl)
+plt.savefig("plot.png")
+#plt.show()
+
