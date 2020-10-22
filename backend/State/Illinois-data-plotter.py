fa46963b4237c707caaccbe099bf417cd97ef3ca
Nakul Iyer <nakulpiyer@gmail.com>
2020-10-21 21:33:28 -0400

    State data to plt

--- backend/State/Illinois-data-plotter.py
+++ backend/State/Illinois-data-plotter.py
@@ -2,20 +2,24 @@ import requests
 import matplotlib.pyplot as plt
 r = requests.get('https://api.covidtracking.com/v1/states/il/daily.json')
 j = r.json()
-str1 = ""
-str2 = ""
+
 xl = []
 yl = []
+totals = []
+j.sort(key=lambda x: x["date"])
 for d in j:
     for key, value in d.items():
         if key == "date":
-            xl.append(value)
-        if key == "positive":
+            xl.append(str(value)[:4] + "-" + str(value)[4:6] + "-" + str(value)[6:])
+        elif key == "positive":
             yl.append(value)
-        str2 = str(key) + ' : ' + str(value) + '\n'
-        str1 += str2
-#print(str1)
-plt.plot(xl, yl)
-plt.savefig("plot.png")
-#plt.show()
+        elif key == "totalTestResults":
+            totals.append(value)
 
+plt.figure(figsize=(18, 10), dpi=180)
+plt.title("Tests and positive cases")
+plt.plot(xl, yl, label="Positive cases")
+plt.plot(xl, totals, label="Number of people tested")
+plt.legend()
+plt.xticks(xl[::20], visible=True, rotation="horizontal")
+plt.savefig("backend/plt/illinois_cases.png")
