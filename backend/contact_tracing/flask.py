6c5a27ab44e4c81b0b14e84848025589dbbc5e19
Nakul Iyer <nakulpiyer@gmail.com>
2020-11-18 00:30:11 -0500

    flask is now centralized

--- backend/contact_tracing/flask.py
+++ backend/contact_tracing/flask.py
@@ -8,11 +8,11 @@ app = Flask(__name__)
 def db_connection():
     conn = None
     try:
-        conn = sqlite3.connect("ct.sqlite") 
-    except: sqlite3.error as e:
+        conn = sqlite3.connect("ct.sqlite")
+    except sqlite3.error as e:
         print(e)
     return conn
-:
+
 #gets a new code from the db
 @app.route("/newCode")
 def newCode():
@@ -20,9 +20,9 @@ def newCode():
     m = SecretHandler()
     return get_new(m)
 
-#checks if person was in contact with an infected person 
+#checks if person was in contact with an infected person
 @app.route("/check")
-def check(codes): 
+def check(codes):
     conn = db_connection()
     m = ContactTracing()
     if check_compromised(m, codes):
@@ -30,16 +30,18 @@ def check(codes):
     else:
         return False
 
-#sends phone's location,current time, and present code to db 
+
 @app.route("/locationSend", methods=["POST"])
-def locationSend(lat, lon, tim, code):
+def post_location(lat, lon, tim, code):
+    """ sends phone's location,current time, and present code to db  """
     conn = db_connection()
     m = Locations()
     report_location(m, code, lat, lon, tim)
 
-#sends infected persons codes to the db
+
 @app.route("/codeSend", methods=["POST"])
-def codeSend(codes):
+def post_compromised_code(codes):
+    """ sends infected persons codes to the db """
     conn = db_connection()
     m = SecretHandler()
     mark_compromised(m, codes)
