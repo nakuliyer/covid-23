0e4245f3aa005bea31c789a552dfc5373d4ec8b3
Nakul Iyer <nakulpiyer@gmail.com>
2020-11-18 00:54:07 -0500

    heroku

--- app.py
+++ app.py
@@ -13,3 +13,8 @@ def ping_pong():
 
 sys.path.append(os.path.abspath(os.path.join(__file__, "../../../")))
 import backend.contact_tracing.flask
+import backend.news.flask_main
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
